11d3a2dea5fd5fb84b2cd292663e101c989b3e41
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2021-07-18 02:10:31 +0100

    Create k_nearest_neighbors.py
    Created skeleton for k nearest neighbors algorithm

---
+++ machine_learning_safari/k_nearest_neighbors.py
@@ -0,0 +1,21 @@
+from .core import NullModel
+
+class KNN(NullModel):
+    """
+    K Nearest Neighbors algorithm
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+
+    Implementation Notes
+    --------------------
+    """
+    def __init__(self, objective):
+        pass
+    def fit(self, X, y):
+        pass
+    def predict(self,X):
+        pass
