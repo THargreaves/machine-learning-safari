ed45e8157b2816601ac5c367b4efc6f2c077a2f5
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2021-07-18 02:09:50 +0100

    Create linear_regression.py
    Created skeleton for OLS linear regression module

---
+++ machine_learning_safari/linear_regression.py
@@ -0,0 +1,22 @@
+from .core import NullModel
+
+class LinearRegression(NullModel):
+    """
+    Ordinary Least Squares Regression model
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+
+    Implementation Notes
+    --------------------
+
+    """
+    def __init__(self, intercept=True):
+        pass
+    def fit(self, X,y):
+        pass
+    def predict(self, X):
+        pass
