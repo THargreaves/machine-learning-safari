c92a7d0ce5306dfc17f67685009bc8d6cefc2822
THargreaves <tim.hargreaves@icloud.com>
2021-10-08 11:49:52 +0100

    Improve CI system

--- noxfile.py
+++ noxfile.py
@@ -1,18 +1,42 @@
 """Automation of unittests, linting, and type-checking."""
+import tempfile
+
 import nox  # type: ignore
 
-nox.options.sessions = 'tests', 'lint', 'mypy'
+nox.options.sessions = 'tests', 'lint', 'mypy', 'xdoctest'
 
 # Parameters
 versions = ['3.9']
 locations = 'src', 'tests'
 
 
+# Helper
+def install_with_constraints(session, *args, **kwargs):
+    """Install specific dependencies using Poetry for static analysis."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            'poetry',
+            'export',
+            '--dev',
+            '--without-hashes',
+            '--format=requirements.txt',
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=versions)
 def tests(session):
     """Perform unittests using pytest."""
     args = session.posargs or ['--cov']
-    session.run('poetry', 'install', external=True)
+    session.run('poetry', 'install', '--no-dev', external=True)
+    install_with_constraints(
+        session,
+        'pytest',
+        'coverage',
+        'pytest-cov',
+    )
     session.run('pytest', *args)
 
 
@@ -20,7 +44,13 @@ def tests(session):
 def lint(session):
     """Lint code using flake8."""
     args = session.posargs or locations
-    session.run('poetry', 'install', external=True)
+    install_with_constraints(
+        session,
+        "flake8",
+        'flake8-docstrings',
+        "flake8-import-order",
+        'darglint',
+    )
     session.run('flake8', *args)
 
 
@@ -28,15 +58,17 @@ def lint(session):
 def mypy(session) -> None:
     """Perform type-checking using mypy."""
     args = session.posargs or locations
-    session.run('poetry', 'install', external=True)
-    session.run("mypy", *args)
+    session.run('poetry', 'install', '--no-dev', external=True)
+    install_with_constraints(session, 'mypy')
+    session.run('mypy', *args)
 
 
 @nox.session(python=versions)
 def xdoctest(session) -> None:
     """Run examples with xdoctest."""
     args = session.posargs or ['all']
-    session.run('poetry', 'install', external=True)
+    session.run('poetry', 'install', '--no-dev', external=True)
+    install_with_constraints(session, 'xdoctest')
     session.run('python', '-m', 'xdoctest', 'machine_learning_safari', *args)
 
 
