998f80d9f75a39315e6199c94138315bc858db7d
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2021-10-13 01:48:57 +0100

    Update linear_regression.py

--- machine_learning_safari/linear_regression.py
+++ machine_learning_safari/linear_regression.py
@@ -1,46 +1,57 @@
-from .core import NullModel
 import numpy as np
-from scipy import linalg
-from scipy import optimize
 
-class LinearRegression(NullModel):
+from mlsafari.core import _SupervisedModel
+
+class LinearRegression(_SupervisedModel):
     """
     Ordinary Least Squares Regression model
 
     Fits a linear model such that coefficients minimize the
     residual sum of squares between the observed target and
     the predicted target.
-
-    Parameters
-    ----------
-    :param intercept:
-
-    Attributes
-    ----------
-    :att X:
-
-    """
-    def __init__(self, intercept=True, positive=False):
-        if not isinstance(intercept, (boolean, int)) and k > 0:
-            raise ValueError("`k` must be a positive integer")
-        self.intercept = intercept
-        self.positive = positive
-
-    def fit(self, X,y):
+     """
+    def __init__(self, intercept=True):
         """
-        Fits model coefficients using OLS estimation
-        :param X: Data matrix of shape (n_samples, n_features)
-        :param y: Target vector of shape (n_samples, 1)
-        :return: self
-        """
+        Initialise the model.
 
+        Args:
+            intercept: Whether or not the model is fitted with an intercept.
+        """
+        # Input validation
+        self._validate_inputs(intercept)
+        # Attributes    
+        self.intercept = intercept 
+        self.coef = None
+        self.resid = None
+        super(LinearRegression, self).__init__()
+
+    @staticmethod
+    def _validate_inputs(intercept):
+        if not isinstance(intercept, bool):
+            raise ValueError("`intercept` must be boolean") 
+
+    def _fit(self, X,y):
+        """
+        Fits model coefficients using OLS estimation.
+
+        Args:
+            X: Data matrix of shape (n_samples, n_features)
+            y: Target vector of shape (n_samples, 1)
+
+        Returns:
+             self
+        """
         # Adding column of ones for models with fitted intercept
-        if intercept:
+        if self.intercept:
             X = np.vstack([X, np.ones(len(X))]).T
-        np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
-
-        self.coef, self.residuals, self.rank, self.singular = linalg.lstsq(X, y)
-        self.coef = self.residuals.T
+        # Computing coefficients
+        self.coef = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
+        self.resid = y - np.dot(X, self.coef)
         return self
-    def predict(self, X):
-        pass
+
+    def _apply(self, X):
+        preds = np.dot(X, self.coef)
+        return preds
+
+    def _inspect(self):
+        raise NotImplementedError
